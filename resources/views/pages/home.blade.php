4967d65d488b334e35f14b48bcaf2a224cc618bf
CJ <acajaja@gmail.com>
2022-04-07 01:39:05 -0400

    Refactored home page using CSS grids.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,18 +5,31 @@
 @endpush
 
 @section('content')
-    <div id="background-bg"></div>
-    <div id="floating-cloud-1"><img alt="" src="http://media.clifjackson.net/images/floating-cloud-BW-94x50.png"></div>
+    <div id="floating-cloud-1">
+        <img alt="" src="http://media.clifjackson.net/images/floating-cloud-BW-94x50.png">
+    </div>
     <div id="left-side-middle-bg"></div>
     <div id="top-right-corner-bg"></div>
 
     <div id="container">
-        <div class="circle" id="circleA"><p><a href="{{ route('web') }}" title="Web">Web</a></p></div>
-        <div class="circle" id="circleB"><p><a href="{{ route('music') }}" title="Music">Music</a></p></div>
-        <div class="in-btwn-box"><a href="{{ route('in-between') }}" title="in between">in between</a></div>
-    </div>
+        <div id="rings-links">
+            <div class="circle" id="circleA">
+                <p>
+                    <a href="{{ route('web') }}" title="Web">Web</a>
+                </p>
+            </div>
+            <div class="circle" id="circleB">
+                <p>
+                    <a href="{{ route('music') }}" title="Music">Music</a>
+                </p>
+            </div>
+            <div class="in-btwn-box">
+                <a href="{{ route('in-between') }}" title="in between">in between</a>
+            </div>
+        </div>
 
-    <div id="my-name">
-        <p>clifjackson.net</p>
+        <div id="my-name">
+            <p>clifjackson.net</p>
+        </div>
     </div>
 @endsection
